271bad4b9acb07994bdc821fbf024fcfed91dc16
fitsum belayneh <fitsu_alt@yahoo.com>
2019-09-04 08:55:43 -0400

    1.5: course information step5

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -29,11 +29,11 @@ const Content = props => {
 };
 
 const Total = props => {
-  // console.log(props);
+  console.log(props);
   return (
     <div>
       <p>
-        Number of exercises{" "}
+        Number of exercises
         {props.parts[0].exercises +
           props.parts[1].exercises +
           props.parts[2].exercises}
@@ -43,28 +43,29 @@ const Total = props => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 7
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 10
-    },
-    {
-      name: "State of a component",
-      exercises: 14
-    }
-  ];
-
-  console.log(course);
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 7
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 10
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
+  // console.log(course);
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
