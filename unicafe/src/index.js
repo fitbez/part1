8a43a0f17ba9f98cece0b80785d12ccf4f9e7405
fitsum belayneh <fitsu_alt@yahoo.com>
2019-09-12 08:08:31 -0400

    1.7: unicafe step2

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -8,6 +8,18 @@ const App = props => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const sumAverage = good * 1 + neutral * 0 + bad * -1;
+  let average;
+  let positive;
+
+  if (sumAverage === 0 && all === 0) {
+    average = 0;
+    positive = 0;
+  } else {
+    average = sumAverage / all;
+    positive = (good / all) * 100;
+  }
 
   return (
     <div>
@@ -19,6 +31,9 @@ const App = props => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   );
 };
