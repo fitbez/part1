e16de951f4a3d96f2ecf73ae7decf8ffed8482df
fitsum belayneh <fitsu_alt@yahoo.com>
2019-09-12 12:13:23 -0400

    1.10: unicafe step5

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -2,32 +2,43 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const Statistics = ({ good, neutral, bad, all, average, positive }) => {
-  // console.log(props)
-
-  if (good === 0 && neutral === 0 && bad === 0) {
-    return (
-      <div>
-        <p>No feedback given</p>
-      </div>
-    );
-  }
+const Statistic = ({ text, value }) => {
   return (
     <div>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <p>
+        {text} {value}
+      </p>
     </div>
   );
 };
 
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+  // console.log(props)
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+  return (
+    <div>
+      <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+      <Statistic text="all" value={all} />
+      <Statistic text="average" value={average} />
+      <Statistic text="positive" value={positive + "%"} />
+    </div>
+  );
+};
+
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
+
 const App = props => {
   // Save clicks of each button to own state
-  console.log(props);
+  // console.log(props);
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -47,9 +58,9 @@ const App = props => {
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button text="good" onClick={() => setGood(good + 1)} />
+      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" onClick={() => setBad(bad + 1)} />
       <Statistics
         good={good}
         bad={bad}
