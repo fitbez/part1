1dee553a32b408a3488d86a5adf55c7560866f61
fitsum belayneh <fitsu_alt@yahoo.com>
2019-09-12 14:12:33 -0400

    1.11: unicafe step6

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -4,11 +4,10 @@ import "./index.css";
 
 const Statistic = ({ text, value }) => {
   return (
-    <div>
-      <p>
-        {text} {value}
-      </p>
-    </div>
+    <tr>
+      <td> {text}</td>
+      <td> {value}</td>
+    </tr>
   );
 };
 
@@ -23,14 +22,16 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
     );
   }
   return (
-    <div>
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral} />
-      <Statistic text="bad" value={bad} />
-      <Statistic text="all" value={all} />
-      <Statistic text="average" value={average} />
-      <Statistic text="positive" value={positive + "%"} />
-    </div>
+    <table>
+      <tbody>
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={all} />
+        <Statistic text="average" value={average} />
+        <Statistic text="positive" value={positive + "%"} />
+      </tbody>
+    </table>
   );
 };
 
